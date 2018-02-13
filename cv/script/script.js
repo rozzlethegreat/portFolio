def0b756d05aa71baaf5b41a57286fce6a166f8c
rozzlethegreat <rosalind@cervante.me>
2018-02-13 21:47:30 +1300

    updated cv

--- cv/script/script.js
+++ cv/script/script.js
@@ -14,21 +14,22 @@ function drawChart() {
     dataTable.addColumn({ type: 'date', id: 'Start' });
     dataTable.addColumn({ type: 'date', id: 'End' });
     dataTable.addRows([
-        [ 'Place', 'Palmerston North', new Date(2012, 2, 2), new Date(2012, 6, 12)],
-        [ 'Place', 'London', new Date(2012, 6, 12), new Date(2012, 10, 28)],
-        [ 'Place', 'Palmerston North', new Date(2012, 10, 28), new Date(2013, 8, 15)],
-        [ 'Place', 'London', new Date(2013, 8, 15), new Date(2014, 2, 1)],
-        [ 'Place', 'Scotland', new Date(2014, 2, 1), new Date(2014, 6, 28)],
-        [ 'Place', 'Holland', new Date(2014, 6, 28), new Date(2015, 7, 5)],
-        [ 'Place', 'Wellington', new Date(2015, 7, 5), new Date(2017, 12, 15)],
-        [ 'Work', 'Pak N Save', new Date(2012, 2, 2), new Date(2012, 6, 12) ],
-        [ 'Work', 'Charity Fundraiser', new Date(2012, 8, 28), new Date(2012, 10, 28) ],
-        [ 'Work', 'Post Haste', new Date(2012, 12, 10), new Date(2013, 8, 15) ],
-        [ 'Work', 'Red 5', new Date(2013, 10, 1), new Date(2014, 2, 1) ],
-        [ 'Work', 'Mad Mex', new Date(2015, 8, 28), new Date(2017, 12, 15) ],
-        [ 'Study', 'KABK', new Date(2014, 7, 1), new Date(2015, 5, 28)],
-        [ 'Study', 'Massey', new Date(2016, 2, 1), new Date(2016, 12, 5)],
-        [ 'Study', 'Yoobee', new Date(2017, 2, 27), new Date(2017, 12, 15)]
+        [ 'Place', 'Palmerston North', new Date(2012, 1, 2), new Date(2012, 5, 12)],
+        [ 'Place', 'London', new Date(2012, 5, 12), new Date(2012, 9, 28)],
+        [ 'Place', 'Palmerston North', new Date(2012, 9, 28), new Date(2013, 7, 15)],
+        [ 'Place', 'London', new Date(2013, 7, 15), new Date(2014, 1, 1)],
+        [ 'Place', 'Scotland', new Date(2014, 1, 1), new Date(2014, 5, 28)],
+        [ 'Place', 'Holland', new Date(2014, 5, 28), new Date(2015, 6, 5)],
+        [ 'Place', 'Wellington', new Date(2015, 6, 5), new Date(2018, 1, 30)],
+        [ 'Work', 'Pak N Save', new Date(2012, 1, 2), new Date(2012, 5, 12) ],
+        [ 'Work', 'Charity Fundraiser', new Date(2012, 7, 28), new Date(2012, 9, 28) ],
+        [ 'Work', 'Post Haste', new Date(2012, 11, 10), new Date(2013, 7, 15) ],
+        [ 'Work', 'Red 5', new Date(2013, 9, 1), new Date(2014, 1, 1) ],
+        [ 'Work', 'Mad Mex', new Date(2015, 7, 28), new Date(2017, 8, 31) ],
+        [ 'Work', 'BNZ', new Date(2017, 9, 1), new Date(2018, 1, 16) ],
+        [ 'Study', 'KABK', new Date(2014, 6, 1), new Date(2015, 4, 28)],
+        [ 'Study', 'Massey', new Date(2016, 1, 1), new Date(2016, 11, 5)],
+        [ 'Study', 'Yoobee', new Date(2017, 1, 27), new Date(2017, 11, 15)]
 
     ]);
 
