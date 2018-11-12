adc1aa5725a3f96a00102a97965644aff22bfe43
rozzlethegreat <rosalind@cervante.me>
2018-11-13 09:48:21 +1300

    took out php and made all html

--- incl/head.php
+++
@@ -1,9 +0,0 @@
-
-<link rel="stylesheet" type="text/css" href="../css/style.css">
-<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
-<link href="../css/lightbox.css" rel="stylesheet">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" href="../images/imagesOfMe/logo.ico">
-
-
-
